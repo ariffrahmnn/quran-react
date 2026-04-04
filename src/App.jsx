de86b8333eae1054a268eae3dbc886510e0323a4
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-04-04 12:15:11 +0700

    Added Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useRef } from 'react';
+import Hero from './components/hero';
+import Footer from './components/footer';
 import QuranList from './pages/QuranList';
 import SurahDetail from './pages/SurahDetail';
 
-function App() {
+export default function App() {
+
+  const quranListRef = useRef(null);
+  const scrollToQuotes = () => {
+    quranListRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<QuranList />} />
-        
+        <Route path="/" element={
+          <>
+            <Hero onExploreClick={scrollToQuotes} />
+            
+            <div ref={quranListRef}>
+              <QuranList />
+            </div>
+          </>
+        } />
         <Route path="/surat/:nomor" element={<SurahDetail />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
-
-export default App;
