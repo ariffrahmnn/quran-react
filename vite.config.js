4258db4ed08ce73e2cdb9f3c387e599341eb1eec
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-04-05 11:08:12 +0700

    Added for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/quran-react/',
+  base: 'https://ariffrahmnn.github.io/quran-react/',
   plugins: [react()],
 })
